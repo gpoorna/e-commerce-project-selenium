5bc2c0a49534ae2d9ae52f218d8d21c845467c79
Admin <gpoornacherry123@gmail.com>
2025-06-19 23:31:27 +0530

    modifed the files

--- TestPractice2.py
+++ TestPractice2.py
@@ -11,10 +11,10 @@ name = "Ravi"
 driver.maximize_window()
 driver.implicitly_wait(10)
 #find_elements used when we have multiple elements with same name
-'''radioButtons1 = driver.find_elements(By.CLASS_NAME, "radioButton")
+radioButtons1 = driver.find_elements(By.CLASS_NAME, "radioButton")
 #click() elements are used when to permform click operation
 radioButtons1[2].click()
-#assert performs avlidation check wheather the condition is true or not
+#assert performs a validation & check wheather the condition is true or not
 #is_selected used to verify wheather elemets are selected or not
 assert radioButtons1[2].is_selected()
 
@@ -33,10 +33,10 @@ for country in countries:
 dropdown = Select(driver.find_element(By.ID, "dropdown-class-example"))
 dropdown.select_by_index(1)
 driver.implicitly_wait(5)
-driver.find_element(By.ID, "checkBoxOption1").click()'''
+driver.find_element(By.ID, "checkBoxOption1").click()
 
-''' Note: window_handles functions return the windows name and stores in the form of list using which we can easily perform the switch operation '''
-'''
+#Note: window_handles functions return the windows name and stores in the form of list using which we can easily perform the switch operation '''
+
 #switching from one window to another window
 current_window = driver.current_window_handle
 print("first window title:" +driver.title)
@@ -45,16 +45,16 @@ driver.find_element(By.ID, "openwindow").click()
 driver.switch_to.window(driver.window_handles[1])
 print("second winodw title:" +driver.title)
 #swictching back to curren t window
-driver.switch_to.window(current_window) '''
+driver.switch_to.window(current_window)
 
 #swiching from one to tab to another tab
-'''driver.find_element(By.LINK_TEXT,"Open Tab").click()
+driver.find_element(By.LINK_TEXT,"Open Tab").click()
 windowsOpened = driver.window_handles
 driver.switch_to.window(windowsOpened[1])
 #Navigates to next window use window_Handles to get name
 driver.find_element(By.LINK_TEXT, "Access all our Courses").click()
 driver.close()
-driver.switch_to.window(windowsOpened[0])'''
+driver.switch_to.window(windowsOpened[0])
 
 #handling the javascript
 '''driver.find_element(By.ID, "name").send_keys(name)
@@ -70,7 +70,7 @@ assert name in alertTextInfo
 alert.dismiss()'''
 
 #mouse hover action
-#Note:ActionChains is used to perform any new Actions
+#Note:ActionChains is used to perform any new Ac   tions
 #move_to_element is used to perform the mouse hover operation
 actions = ActionChains(driver)
 actions.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
